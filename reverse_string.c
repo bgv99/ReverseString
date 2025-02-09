68b75f707eeeb6a0a632684d84a1bf940d5221ae
bgv99 <197402987+bgv99@users.noreply.github.com>
2025-02-09 18:28:51 +0300

    Source Upload

---
+++ reverse_string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void reverseString(char *string)
+{
+    int string_length = strlen(string);
+
+    for(int i = string_length; i >= 0; i--)
+    {
+        printf("%c", string[i]);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    char *string = (char *) malloc(sizeof(char) * 1024);
+
+    if(string == NULL)
+    {
+        return 1;
+    }
+
+    printf("Enter a String: ");
+    fgets(string, sizeof(char) * 1024, stdin);
+    reverseString(string);
+
+    return 0;
+}
